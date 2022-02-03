41bb3e42f35bc4bc917357cdecee5d83975ab092
Bertrand Thomas <bertrand@devpro.fr>
2022-02-03 11:05:26 +0100

    Add InMemory cache in service collections

--- src/Application/DependencyInjection/WebApplicationServicesExtensions.cs
+++ src/Application/DependencyInjection/WebApplicationServicesExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RabbidsIncubator.ServiceNowClient.Infrastructure.InMemory.DependencyInjection;
-using RabbidsIncubator.ServiceNowClient.Infrastructure.ServiceNowRestClient;
 using RabbidsIncubator.ServiceNowClient.Infrastructure.ServiceNowRestClient.DependencyInjection;
 
 namespace RabbidsIncubator.ServiceNowClient.Application.DependencyInjection
@@ -14,8 +13,8 @@ namespace RabbidsIncubator.ServiceNowClient.Application.DependencyInjection
             params AutoMapper.Profile[] additionalProfiles)
         {
             services.AddAutoMapperConfiguration(additionalProfiles);
-            services.AddInMemoryRepositories();
-            services.AddServiceNowRestClientRepositories(configuration.GetSection("ServiceNow:RestApi").Get<ServiceNowRestClientConfiguration>());
+            services.AddInMemoryRepositories(configuration.GetSection("Cache:InMemory").Get<Infrastructure.InMemory.InMemoryConfiguration>());
+            services.AddServiceNowRestClientRepositories(configuration.GetSection("ServiceNow:RestApi").Get<Infrastructure.ServiceNowRestClient.ServiceNowRestClientConfiguration>());
             services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
--- src/Infrastructure.InMemory/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Infrastructure.InMemory/DependencyInjection/ServiceCollectionExtensions.cs
@@ -9,15 +9,21 @@ namespace RabbidsIncubator.ServiceNowClient.Infrastructure.InMemory.DependencyIn
         /// <summary>
         /// Register in-memory services.
         /// </summary>
+        /// <typeparam name="T"></typeparam>
         /// <param name="services"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static IServiceCollection AddInMemoryRepositories(this IServiceCollection services)
+        public static IServiceCollection AddInMemoryRepositories<T>(this IServiceCollection services, T configuration)
+            where T : InMemoryConfiguration
         {
             if (services == null)
             {
                 throw new ArgumentNullException(nameof(services));
             }
+
+            services.AddSingleton(configuration);
+
+            services.TryAddSingleton<Microsoft.Extensions.Caching.Memory.IMemoryCache, Microsoft.Extensions.Caching.Memory.MemoryCache>();
 
             services.TryAddTransient<Domain.Repositories.ICacheRepository, Repositories.InMemoryCacheRepository>();
 
